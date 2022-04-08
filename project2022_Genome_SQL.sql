d016e08d260efd2dd3374952078014325f2692a9
jesusaldanamartin <jesus.aldana@uma.es>
2022-04-08 13:22:39 +0200

    Ahora si esta el sql

--- project2022_Genome_SQL.sql
+++ project2022_Genome_SQL.sql
@@ -40,9 +40,8 @@ CREATE TABLE IF NOT EXISTS `Project2022_GenomicBD`.`Genome` (
   `idGenome` INT NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(45) NOT NULL DEFAULT 'Unnamed',
   `Assembly` VARCHAR(45) NOT NULL,
-  `Lab` VARCHAR(45) NULL DEFAULT 'Unknown',
   `Release_date` DATE NULL,
-  `Release number` INT NOT NULL,
+  `Release number` INT NOT NULL DEFAULT -1,
   `Organism_idOrganism` INT NOT NULL,
   PRIMARY KEY (`idGenome`),
   UNIQUE INDEX `idGenoma_UNIQUE` (`idGenome` ASC) VISIBLE,
@@ -191,7 +190,35 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `Project2022_GenomicBD`;
-INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (1, 'Human', 'Homo', 'Homo sapiens');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (1, 'human', 'Homininae', 'Homo sapiens');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (2, 'mouse', 'Murinae', 'Mus musculus');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (3, 'dog', 'Laurasiatheria', 'Canis lupus');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (4, 'cattle', 'Bovidae', 'Bos taurus');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (5, 'sheep', 'Bovidae', 'Ovis aries');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (6, 'fruit fly', 'Diptera', 'Drosophila melanogaster');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (7, 'nematode', 'Ecdysozoa', 'Caenorhabditis');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (8, 'arabidopsis', 'rosids', 'Arabidopsis thaliana');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (9, 'zebrafish', 'Euteleostomi', 'Danio rerio');
+INSERT INTO `Project2022_GenomicBD`.`Organism` (`idOrganism`, `Common name`, `Family`, `Spieces`) VALUES (10, 'rice', 'Poaceae', 'Oryza sativa');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `Project2022_GenomicBD`.`Genome`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `Project2022_GenomicBD`;
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (1, 'Homo sapiens', 'GRCh38.p14', '2021-11-22', 109, 1);
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (2, 'Mus musculus', 'GRCm39', '2020-09-22', 109, 2);
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (3, 'Canis lupus', 'ROS_Cfam_1.0', '2021-01-08', 106, 3);
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (4, 'Bos taurus', 'ARS-UCD1.2', '2018-05-11', 106, 4);
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (5, 'Ovis aries', 'ARS-UI_Ramb_v2.0', '2021-06-03', 104, 5);
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (6, 'Drosophila melanogaster', 'RELEASE 6 plus ISO1 MT', '2014-08-08', DEFAULT, 6);
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (7, 'Caenorhabditis', 'WBcel235', '2013-03-04', DEFAULT, 7);
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (8, 'Arabidopsis thaliana', 'TAIR10.1', '2018-06-04', DEFAULT, 8);
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (9, 'Danio rerio', 'GRCz11', '2017-06-26', 106, 9);
+INSERT INTO `Project2022_GenomicBD`.`Genome` (`idGenome`, `Name`, `Assembly`, `Release_date`, `Release number`, `Organism_idOrganism`) VALUES (10, 'Oryza sativa', 'IRGSP-1.0', '2018-08-07', 102, 10);
 
 COMMIT;
 
