670c55e38864d7414dfa5a57f4920df4fd60e36d
JCruiz15 <100194996+JCruiz15@users.noreply.github.com>
2022-04-17 14:40:21 +0200

    Delete SimpleQuery3.sql

--- SimpleQuery3.sql
+++
@@ -1,5 +0,0 @@
-SELECT Gene.Name as Gene, Sequence.Sequence, Sequence.size ,Protein.Name as Protein, Protein.Function 
-FROM Gene, Sequence, Protein 
-WHERE Gene.Name = 'TP53' 
-	AND Protein.idProtein = Sequence.Protein_idProtein 
-	AND Sequence.Gene_idGene = Gene.idGene;
