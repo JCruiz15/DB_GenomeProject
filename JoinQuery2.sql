3f6f2228c0b364f80253344f1a7aba2f96ee3396
JCruiz15 <100194996+JCruiz15@users.noreply.github.com>
2022-04-17 14:39:54 +0200

    Delete JoinQuery2.sql

--- JoinQuery2.sql
+++
@@ -1,6 +0,0 @@
-SELECT Genome.Name, Genome.Assembly, Chromosome.ChromosomeNum, Bookshelf.Book_idBook, Book.TItle FROM Genome
-INNER JOIN Chromosome ON Genome.idGenome = Chromosome.Genome_idGenome
-INNER JOIN Bookshelf ON Genome.idGenome = Bookshelf.Genome_idGenome
-INNER JOIN Book ON Bookshelf.Book_idBook = Book.idBook;
-
-
